43befded25572663a23bcc999fdc6e1775e31ca4
藤川英悟 <efg.river@gmail.com>
2020-02-23 15:47:31 +0900

    Update sample-geojson.js

--- sample-geojson.js
+++ sample-geojson.js
@@ -1131,3 +1131,46 @@ var campus = {
 ]
 };
 
+var map = L.map('map').setView([26.1202, 127.7025], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWlnb2Z1amlrYXdhIiwiYSI6ImNrNjJia3cxYjBhbjczb24yY244eXVvYmQifQ.uMPaUVKQZQwA75tf49Pk_Q', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/light-v9',
+  tileSize: 512,
+  zoomOffset: -1
+}).addTo(map);
+
+
+function onEachFeature(feature, layer) {
+  var popupContent = "<p>I started out as a GeoJSON " +
+      feature.geometry.type + ", but now I'm a Leaflet vector!</p>";
+
+  if (feature.properties && feature.properties.popupContent) {
+    popupContent += feature.properties.popupContent;
+  }
+
+  layer.bindPopup(popupContent);
+}
+
+L.geoJSON([campus], {
+
+  style: function (feature) {
+    return feature.properties && feature.properties.style;
+  },
+
+  onEachFeature: onEachFeature,
+
+  pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, {
+      radius: 8,
+      fillColor: "#ff7800",
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+    });
+  }
+}).addTo(map);
+
